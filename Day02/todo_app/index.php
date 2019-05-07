58cdd8c3c0654881ec51bf8403802f1330430d80
Ábel Kalamár <abel.kalamar@gmail.com>
2019-05-07 12:36:48 +0200

    feat(todoApp) initial commit

---
+++ Day02/todo_app/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ToDoApp</title>
+</head>
+
+<body>
+  <h1>ToDo App</h1>
+  <?php include 'todoApp.php'; ?>
+</body>
+
+</html>
